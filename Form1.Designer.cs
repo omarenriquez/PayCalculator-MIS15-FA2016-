3e1136518837bd19a8c87fde5535afc2d674fa45
NinjaBoo <NinjaBoo@DESKTOP-6ADU6Q3>
2016-10-18 00:26:13 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtEmployeeType = new System.Windows.Forms.TextBox();
+            this.txtHoursWorked = new System.Windows.Forms.TextBox();
+            this.txtSalesRevenue = new System.Windows.Forms.TextBox();
+            this.txtSalesCommission = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotalPay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(39, 200);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(151, 200);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtEmployeeType
+            // 
+            this.txtEmployeeType.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtEmployeeType.Location = new System.Drawing.Point(151, 27);
+            this.txtEmployeeType.Name = "txtEmployeeType";
+            this.txtEmployeeType.Size = new System.Drawing.Size(100, 20);
+            this.txtEmployeeType.TabIndex = 0;
+            // 
+            // txtHoursWorked
+            // 
+            this.txtHoursWorked.Location = new System.Drawing.Point(151, 53);
+            this.txtHoursWorked.Name = "txtHoursWorked";
+            this.txtHoursWorked.Size = new System.Drawing.Size(100, 20);
+            this.txtHoursWorked.TabIndex = 1;
+            // 
+            // txtSalesRevenue
+            // 
+            this.txtSalesRevenue.Location = new System.Drawing.Point(151, 79);
+            this.txtSalesRevenue.Name = "txtSalesRevenue";
+            this.txtSalesRevenue.Size = new System.Drawing.Size(100, 20);
+            this.txtSalesRevenue.TabIndex = 2;
+            // 
+            // txtSalesCommission
+            // 
+            this.txtSalesCommission.Location = new System.Drawing.Point(151, 105);
+            this.txtSalesCommission.Name = "txtSalesCommission";
+            this.txtSalesCommission.ReadOnly = true;
+            this.txtSalesCommission.Size = new System.Drawing.Size(100, 20);
+            this.txtSalesCommission.TabIndex = 5;
+            this.txtSalesCommission.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Employee Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hours Worked";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Sales Revenue";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Sales Commission";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total Pay";
+            // 
+            // txtTotalPay
+            // 
+            this.txtTotalPay.Location = new System.Drawing.Point(151, 131);
+            this.txtTotalPay.Name = "txtTotalPay";
+            this.txtTotalPay.ReadOnly = true;
+            this.txtTotalPay.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalPay.TabIndex = 11;
+            this.txtTotalPay.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtTotalPay);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSalesCommission);
+            this.Controls.Add(this.txtSalesRevenue);
+            this.Controls.Add(this.txtHoursWorked);
+            this.Controls.Add(this.txtEmployeeType);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtEmployeeType;
+        private System.Windows.Forms.TextBox txtHoursWorked;
+        private System.Windows.Forms.TextBox txtSalesRevenue;
+        private System.Windows.Forms.TextBox txtSalesCommission;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTotalPay;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsValidData())
+                {
+                    string employeeType = txtEmployeeType.Text;
+                    decimal hoursWorked = Convert.ToDecimal(txtHoursWorked.Text);
+                    decimal salesRevenue = Convert.ToDecimal(txtSalesRevenue.Text);
+
+                    decimal compensation = 0;
+                    decimal totalPay = 0;
+                    decimal commissionForSalaried = .02m;
+                    decimal commissionForHourly = .01m;
+
+                    if (employeeType == "S")
+                    {
+                        compensation = (salesRevenue * commissionForSalaried);
+                        totalPay = compensation + 300;
+                    }
+                    else if (employeeType == "C")
+                    {
+                        compensation = (salesRevenue * commissionForHourly);
+                        totalPay = compensation + (hoursWorked * 12);
+                    }
+                    else if (employeeType == "N")
+                    {
+                        compensation = 0;
+                        totalPay = compensation + (hoursWorked * 16);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a correct Employee Type \n\n" + 
+                            "S = Salaried \n C = Hourly Commissioned \n N = Houlry Non-Commision",
+                            "Entry Error");
+                        txtEmployeeType.Focus();
+                    }
+
+
+                    txtEmployeeType.Text = employeeType.ToString();
+                    txtHoursWorked.Text = hoursWorked.ToString();
+                    txtSalesRevenue.Text = salesRevenue.ToString();
+                    txtSalesCommission.Text = compensation.ToString("c");
+                    txtTotalPay.Text = totalPay.ToString("c");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" +
+                    ex.GetType().ToString() + "\n" +
+                    ex.StackTrace, "Exception");
+
+            }
+        }
+
+        public bool IsValidData()
+        {
+            return
+                IsPresent(txtEmployeeType, "Employee Type") &&
+                
+                IsPresent(txtHoursWorked, "Hours Worked") &&
+                IsDecimal(txtHoursWorked, "Hours Worked") &&
+                IsWithinRange(txtHoursWorked, "Hours Worked", 0, 80) &&
+
+                IsPresent(txtSalesRevenue, "Sales Revenue") &&
+                IsDecimal(txtSalesRevenue, "Sales Revenue") &&
+                IsWithinRange(txtSalesRevenue, "Sales Revenue", 0, 100000);
+        }
+
+        public bool IsPresent(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsDecimal(TextBox textBox, string name)
+        {
+            decimal number = 0m;
+            if (Decimal.TryParse(textBox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a decimal value.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+
+        public bool IsWithinRange(TextBox textBox, string name, decimal min, decimal max)
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if (number < min || number > max)
+            {
+                MessageBox.Show(name + " must be netween " + min + " and " + max + " .", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        
+        
+    }
+}
